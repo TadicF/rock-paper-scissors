20a271769e9b051da2adae69907b5ff47440077c
Filip Tadic <fike300705@gmail.com>
2024-10-03 03:18:13 +0200

    Added a function that plays one round and picks a winner

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let humanSchore = 0;
+let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
@@ -21,8 +21,35 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Enter your choice (Rock, Papper, Scissors): ");
+  let humanChoice = prompt("Enter your choice (Rock, Paper, Scissors): ");
   humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
   return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+  if ((humanChoice == "Rock" && computerChoice == "Scissors")
+   || (humanChoice == "Paper" && computerChoice == "Rock") 
+   || (humanChoice == "Scissors" && computerChoice == "Paper")) 
+   {
+    humanScore++;
+    console.log(`You won! ${humanChoice} beats ${computerChoice}`)
+   }
+
+  else if((computerChoice == "Rock" && humanChoice == "Scissors") 
+      || (computerChoice == "Paper" && humanChoice == "Rock") 
+      || (computerChoice == "Scissors" && humanChoice == "Paper")) 
+  {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+  }
+  else {
+    console.log(`It's a draw!`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
 
